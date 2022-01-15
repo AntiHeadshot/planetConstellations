68f2a6fce408244c3c2db2eb392563ced8b6f1d9
Tobias Dahmen <alias.Slash@yahoo.de>
2022-01-15 15:49:06 +0100

    updated stuff

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 var isDebug = window.location.port ? true : false;
 
-document.addEventListener('gesturestart', function (e) {
+document.addEventListener('gesturestart', function(e) {
     e.preventDefault();
 });
 
-Date.prototype.addDays = function (days) {
-    var date = new Date(this.valueOf());
-    date.setDate(date.getDate() + days);
-    return date;
-}
-Date.prototype.addHours = function (h) {
+Date.prototype.addHours = function(h) {
     this.setTime(this.getTime() + (h * 60 * 60 * 1000));
     return this;
 }
-Date.prototype.addSeconds = function (s) {
+Date.prototype.addSeconds = function(s) {
     this.setTime(this.getTime() + (s * 1000));
     return this;
+}
+Date.prototype.addDays = function(days) {
+    return date.addHours(days * 24);
 }
 
 function GetTrueSize(s) {
@@ -29,6 +27,7 @@ function GetLogSize(s) {
 var sizeFunction = GetLogSize;
 
 var canavas;
+var cam;
 
 var planets = [];
 var scaling = 1;
@@ -38,7 +37,7 @@ function updateCanvas() {
     scaling = Math.min(width, height) / (maxDist * 2);
 }
 
-window.onresize = function () {
+window.onresize = function() {
     logData.resize = { iw: window.innerWidth, ih: window.innerHeight };
     updateCanvas()
 }
@@ -51,7 +50,7 @@ function Sleep(milliseconds) {
 
 function orientationChanged() {
     const timeout = 1000;
-    return new window.Promise(function (resolve) {
+    return new window.Promise(function(resolve) {
         let height0 = window.innerHeight;
         let i = 0;
         do {
@@ -65,28 +64,17 @@ function orientationChanged() {
     });
 }
 
-window.onorientationchange = function () {
+window.onorientationchange = function() {
     logData.orientation++;
     //alert(window.orientation);
-    orientationChanged().then(function () {
+    orientationChanged().then(function() {
         isPortrait = window.orientation % 180;
         updateCanvas();
     });
 };
 
 var font;
-var lightMap;
 var maxDist;
-var skyBoxTxt;
-var skyBox;
-
-function preload() {
-    font = loadFont('The_Bellovia_Sans.ttf');
-    //lightMap = loadImage('images/lightMap.png');
-    //https://tools.wwwtyro.net/space-3d/index.html#animationSpeed=0.24599640845243664&fov=150&nebulae=false&pointStars=true&resolution=4096&seed=5ep4a2tl2740&stars=true&sun=false
-    skyBoxTxt = loadImage('images/cubemap.png');
-    skyBox = loadModel('images/skybox.obj');
-}
 
 function renderPlanet(p) {
     lightFalloff(1, 0, 0);
@@ -96,61 +84,58 @@ function renderPlanet(p) {
     sphere(p.GetSize());
 };
 
-function InitPlanet(planet, sizeInKm, color){
+function InitPlanet(planet, sizeInKm, color) {
 
     planet.GetSize = () => sizeFunction(sizeInKm / 2);
     planet.color = color;
-    planet.render = ()=>renderPlanet(planet);
+    planet.render = () => renderPlanet(planet);
 
     return planet;
 }
 
 function setup() {
     canvas = createCanvas(window.innerWidth, window.innerHeight, WEBGL);
-    perspective(PI / 3.0, width / height, 0.1, 5000);
+    cam = createCamera();
+    cam.perspective(PI / 3.0, width / height, 0.1, 100000);
+
+    cam.lookAt(0, 0, 0);
+    cam.setPosition(0, -(height / 2) / tan(PI / 6), 0);
+    cam.tilt(HALF_PI);
 
     colorMode(HSL, 360, 100, 100);
 
-    planets.push(InitPlanet(Astronomy.Sun,696340,color('white')));
-    planets.push(InitPlanet(Astronomy.Mercury,2439.7,color(25, 84, 81)));
-    planets.push(InitPlanet(Astronomy.Venus,6051.8,color(0, 100, 91)));
-    planets.push(InitPlanet(Astronomy.Earth,6371,color(208, 100, 81)));
-    planets.push(InitPlanet(Astronomy.Moon,1737.4,color(200, 100, 99)));
-    planets.push(InitPlanet(Astronomy.Mars,3389.5,color(10, 96, 65)));
-    planets.push(InitPlanet(Astronomy.Jupiter,69911,color(35, 100, 80)));
-    planets.push(InitPlanet(Astronomy.Saturn,58232,color(59, 100, 75)));
-    planets.push(InitPlanet(Astronomy.Uranus,25362,color(216, 100, 81)));
-    planets.push(InitPlanet(Astronomy.Neptune,24622,color(230, 70, 70)));
-    planets.push(InitPlanet(Astronomy.Pluto,1188.3,color(213, 100, 96)));
+    loadFont('The_Bellovia_Sans.ttf', f => font = f);
+
+    planets.push(InitPlanet(Astronomy.Sun, 696340, color('white')));
+    planets.push(InitPlanet(Astronomy.Mercury, 2439.7, color(25, 84, 81)));
+    planets.push(InitPlanet(Astronomy.Venus, 6051.8, color(0, 100, 91)));
+    planets.push(InitPlanet(Astronomy.Earth, 6371, color(208, 100, 81)));
+    planets.push(InitPlanet(Astronomy.Moon, 1737.4, color(200, 100, 99)));
+    planets.push(InitPlanet(Astronomy.Mars, 3389.5, color(10, 96, 65)));
+    planets.push(InitPlanet(Astronomy.Jupiter, 69911, color(35, 100, 80)));
+    planets.push(InitPlanet(Astronomy.Saturn, 58232, color(59, 100, 75)));
+    planets.push(InitPlanet(Astronomy.Uranus, 25362, color(216, 100, 81)));
+    planets.push(InitPlanet(Astronomy.Neptune, 24622, color(230, 70, 70)));
+    planets.push(InitPlanet(Astronomy.Pluto, 1188.3, color(213, 100, 96)));
+
+    Astronomy.Sun.render = function() {
+        emissiveMaterial(38, 100, 65);
+        sphere(this.GetSize());
+    };
 
     var day = Astronomy.DayValue(new Date());
-
-    maxDist = 0;
-    for (var planet of planets)
-        maxDist = Math.max(planet.DistanceFromSun(day), maxDist);
-
-    Astronomy.Sun.render = function () {
-        emissiveMaterial(38, 100, 65);
-        renderPlanet(this);
-    };
+    maxDist = Math.max(...planets.map(p => p.DistanceFromSun(day)));
 
     scaling = Math.min(width, height) / (maxDist * 2);
 
     var mc = new Hammer.Manager(document.body);
 
-    // add to the Manager
-
-    // create a pinch and rotate recognizer
-    // these require 2 pointers
     var pinch = new Hammer.Pinch();
     var rotate = new Hammer.Rotate();
-
-    // we want to detect both the same time
 
     pinch.recognizeWith(rotate);
 
     mc.add([pinch, rotate]);
-
 
     mc.on('pinchstart', e => {
         pf2 = pf;
@@ -161,7 +146,7 @@ function setup() {
     });
 
     mc.on("pinchin pinchout", e => {
-        pf = Math.min(Math.max(1, e.scale * pf2), 300);
+        pf = Math.min(Math.max(1, e.scale * pf2), 50);
     });
     mc.on("rotatemove", e => {
         var angDist = rot - e.rotation;
@@ -172,15 +157,21 @@ function setup() {
 
 }
 
-var logData = { orientation: 0 };
+var logData = {};
 
 var rot = 0;
 var date = new Date();
 
-var pf = 1;
+var pf = 30;
 var pf2 = 1;
 
 function draw() {
+    logData.pf = pf;
+
+    cam.perspective(PI / (3.0 * pf), width / height, 0.1, 100000);
+
+    var ddDist = cam.eyeY + ((height / 2) / tan(PI / (6.0 * pf)));
+
     height = canvas.elt.clientHeight;
     width = canvas.elt.clientWidth;
 
@@ -192,9 +183,42 @@ function draw() {
 
     //var date = new Date().addDays(frameCount).addHours(frameCount / 60 * 24);
     var day = Astronomy.DayValue(date);
-    
-    //orbitControl();
 
+    rotateX(HALF_PI);
+
+    print(ddDist);
+
+    push();
+    translate(0, 0, -ddDist);
+    drawUi();
+    pop();
+
+    push();
+
+    noStroke();
+
+    scale(scaling);
+
+    var sunPos = Astronomy.Sun.EclipticCartesianCoordinates(day);
+    //pointLight(255, 255, 255, sunPos.x, sunPos.z, sunPos.y);
+    for (var planet of planets) {
+        push();
+        var position = planet.EclipticCartesianCoordinates(day);
+
+        if (planet !== Astronomy.Sun) {
+            directionalLight(255, 255, 255, position.x - sunPos.x, position.z - sunPos.z, -(position.y - sunPos.y));
+        }
+        translate(position.x, -position.y, position.z);
+        planet.render()
+        noLights();
+
+        pop();
+    }
+    pop();
+
+}
+
+function drawUi() {
     push();
     stroke('red');
     strokeWeight(2);
@@ -223,43 +247,59 @@ function draw() {
 
         textSize(12);
         if (isDebug)
-            //text(JSON.stringify(logData, null, '\t'), -width / 2, -height / 2 + 40, width, height - 40);
+        //text(JSON.stringify(logData, null, '\t'), -width / 2, -height / 2 + 40, width, height - 40);
             text(JSON.stringify(logData, null, '\t'), 0, 0, width, height - 40);
         pop();
     }
+}
 
+function AddSphere(x, y, z, s) {
     push();
-    perspective(PI / (3.0 * pf), width / height, 0.1, 5000);
-
-    rotateX(HALF_PI)
-
-    noStroke();
-
-    ambientMaterial(255);
-    scale(scaling);
-
-    var sunPos = Astronomy.Sun.EclipticCartesianCoordinates(day);
-
-    pointLight(255, 255, 255, sunPos.x, sunPos.z, sunPos.y);
-    for (var planet of planets) {
-        push();
-
-        var position = planet.EclipticCartesianCoordinates(day);
-
-        translate(position.x, position.z, position.y);
-        planet.render()
-
-        pop();
-    }
+    translate(x, y, z);
+    sphere(s);
     pop();
+}
 
-    push();
-    
-    noStroke();
+// var mX;
+// var mY;
 
-    scale(scaling* maxDist*2);
-    texture(skyBoxTxt);
-    model(skyBox);
+// function touchMoved() {
+//     if (mX !== undefined) {
+//         cam.pan((mX - mouseX) / 1000);
+//         cam.tilt((mouseY - mY) / 1000);
 
-    pop();
+//         console.log(cam);
+//     }
+//     mX = mouseX;
+//     mY = mouseY;
+// }
+
+// function touchEnded() {
+//     mX = undefined;
+//     mY = undefined;
+// }
+
+window.addEventListener('wheel', function(event) {
+    event.preventDefault();
+}, { passive: false });
+
+function mouseWheel(event) {
+    if (crtlPressed)
+        pf = Math.min(Math.max(1, pf * (1 + event.delta / 10000)), 50);
+    else
+        date = date.addDays(Math.sign(event.delta) * 0.5);
+}
+
+var crtlPressed = false;
+
+function keyPressed() {
+    if (keyCode === CONTROL)
+        crtlPressed = true;
+    print(keyCode);
+}
+
+function keyReleased() {
+    if (keyCode === CONTROL)
+        crtlPressed = false;
+    print(keyCode);
 }
